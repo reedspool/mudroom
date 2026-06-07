1e7afe09c2891c599a320a93f9bbd1b026a1332a
Reed Spool <reedwith2es@gmail.com>
2026-06-06 22:47:01 -0700

    More incremental progress on template

--- query-engine/src/template.ts
+++ query-engine/src/template.ts
@@ -1,7 +1,9 @@
-import { parse, HTMLElement } from "node-html-parser";
+import { parse, HTMLElement, Node, NodeType } from "node-html-parser";
+import { TreeWalker, NodeFilter } from "./tree-walker.ts";
 interface InputsInterface {
   Has(key: string): boolean;
   Get(key: string): string;
+  Set(key: string, value: string): void;
 }
 
 export class Inputs {
@@ -18,27 +20,86 @@ export class Inputs {
   }
 }
 
+function assertIsHTMLElement(a: Node): asserts a is HTMLElement {
+  if (a.nodeType !== NodeType.ELEMENT_NODE) {
+    throw new Error(`Treewalker showed a non-HTMLElement Node '${a}'`);
+  }
+}
+
+function asHTMLElement(a: Node): HTMLElement {
+  assertIsHTMLElement(a);
+  return a;
+}
+
+function asHTMLElementOrNull(a: Node | null): HTMLElement | null {
+  if (a === null) return a;
+  assertIsHTMLElement(a);
+  return a;
+}
+
 export function template(
   html: string,
   inputs: InputsInterface,
   query: (expression: string) => unknown = () => {},
 ): string {
-  let dom: HTMLElement | null = parse(html);
-  const root = dom;
-  if (dom === null) return "";
-  if (inputs.Has("rootSelector"))
-    dom = dom.querySelector(inputs.Get("rootSelector"));
-  if (dom === null) return "";
-  if (dom.tagName === "R-") {
-    if (dom.hasAttribute("content")) {
-      const result = query(dom.getAttribute("content")!) + "";
-      dom.replaceWith(result);
-      return root.toString();
+  let root: HTMLElement | null = parse(html);
+  if (inputs.Has("rootSelector")) {
+    root = root.querySelector(inputs.Get("rootSelector"))!;
+  }
+  const treeWalker = new TreeWalker(root, NodeFilter.SHOW_ELEMENT);
+  let element = treeWalker.currentNode;
+  assertIsHTMLElement(element);
+  if (element === null) return "";
+
+  let alreadySetForNextIteration: HTMLElement | null = null;
+  do {
+    element = treeWalker.currentNode;
+    assertIsHTMLElement(element);
+
+    if (element === null) return "";
+    if (element.tagName === "R-") {
+      if (element.hasAttribute("content")) {
+        const result = query(element.getAttribute("content")!) + "";
+        const shouldEvaluateChildren = false;
+        const shouldKeepContents = false;
+
+        // If this is root... everything's different. This won't be captured, because the treeWalker cannot just cannot.
+        if (root === element) {
+          return result;
+        } else {
+          element.after(result);
+        }
+
+        if (shouldEvaluateChildren) {
+          alreadySetForNextIteration = asHTMLElementOrNull(
+            treeWalker.nextNode(),
+          );
+        } else {
+          alreadySetForNextIteration = asHTMLElementOrNull(
+            treeWalker.nextNodeNotChildren(),
+          );
+        }
+        if (shouldKeepContents) {
+          element.after(...element.childNodes);
+        }
+
+        element.remove();
+      }
     }
-  }
-  if (dom.hasAttribute("x-content")) {
-    dom.innerHTML = query(dom.getAttribute("x-content")!) + "";
-    dom.removeAttribute("x-content");
-  }
-  return dom.toString();
+    if (element.tagName === "SET-") {
+      for (const [key, value] of Object.entries(element.attributes)) {
+        const result = query(value) + "";
+        inputs.Set(key, result);
+      }
+      alreadySetForNextIteration = asHTMLElementOrNull(
+        treeWalker.nextNodeNotChildren(),
+      );
+      element.remove();
+    }
+    if (element.hasAttribute("x-content")) {
+      element.innerHTML = query(element.getAttribute("x-content")!) + "";
+      element.removeAttribute("x-content");
+    }
+  } while (treeWalker.nextNode() || alreadySetForNextIteration);
+  return root.toString();
 }
--- query-engine/src/template_test.ts
+++ query-engine/src/template_test.ts
@@ -2,48 +2,77 @@ import { assertEquals } from "jsr:@std/assert";
 import { assertSpyCalls, spy } from "jsr:@std/testing/mock";
 import { template, Inputs } from "./template.ts";
 
-Deno.test("Simple HTML has no effect", () => {
-  const input = '<a href="https://example.com"><span>F</span></a>';
-  const result = template(input, new Inputs());
-  assertEquals(result, input);
-});
-
-Deno.test("Parser removes incorrect closing tags, apparently?", () => {
-  const input = "<a href=https://example.com>F</span></a>";
-  const expected = "<a href=https://example.com>F</a>";
-  const result = template(input, new Inputs());
-  assertEquals(result, expected);
-});
-
-Deno.test("Root selector", () => {
-  const input = '<a href="https://example.com"><span>F</span></a>';
-  const expected = "<span>F</span>";
-  const inputs = new Inputs();
-  inputs.Set("rootSelector", "span");
-  const result = template(input, inputs);
-  assertEquals(result, expected);
-});
-
-Deno.test("x-content", () => {
-  const input = '<span x-content="5+2" />';
-  const expected = "<span>7</span>";
-  const inputs = new Inputs();
-  const querySpy = spy((_) => 7);
-  inputs.Set("rootSelector", "span");
-  const result = template(input, inputs, querySpy);
-  assertSpyCalls(querySpy, 1);
-  assertEquals(querySpy.calls[0].args, ["5+2"]);
-  assertEquals(result, expected);
-});
-
-Deno.test("<r- content=...>", () => {
-  const input = '<r- content="5+2" />';
-  const expected = "7";
-  const inputs = new Inputs();
-  const querySpy = spy((_) => 7);
-  inputs.Set("rootSelector", "r-");
-  const result = template(input, inputs, querySpy);
-  assertSpyCalls(querySpy, 1);
-  assertEquals(querySpy.calls[0].args, ["5+2"]);
-  assertEquals(result, expected);
+Deno.test("templating", async (t) => {
+  const a: Array<{
+    name: string;
+    input: string;
+    expected: string;
+    inputs: Record<string, string>;
+    expectedQueryCallArgs: Array<[string]>;
+  }> = [
+    {
+      name: "Simple HTML has no effect",
+      input: '<a href="https://example.com"><span>F</span></a>',
+      expected: '<a href="https://example.com"><span>F</span></a>',
+      inputs: {},
+      expectedQueryCallArgs: [],
+    },
+    {
+      name: "Parser removes incorrect closing tags, apparently?",
+      input: "<a href=https://example.com>F</span></a>",
+      expected: "<a href=https://example.com>F</a>",
+      inputs: {},
+      expectedQueryCallArgs: [],
+    },
+    {
+      name: "Root selector",
+      input: '<a href="https://example.com"><span>F</span></a>',
+      expected: "<span>F</span>",
+      inputs: { rootSelector: "span" },
+      expectedQueryCallArgs: [],
+    },
+    {
+      name: "x-content",
+      input: '<span x-content="5+2" />',
+      expected: "<span>7</span>",
+      inputs: { rootSelector: "span" },
+      expectedQueryCallArgs: [["5+2"]],
+    },
+    {
+      name: "nested x-content",
+      input: '<span><span x-content="5+2" /></span>',
+      expected: "<span><span>7</span></span>",
+      inputs: { rootSelector: "span" },
+      expectedQueryCallArgs: [["5+2"]],
+    },
+    {
+      name: "<r- content=...>",
+      input: '<r- content="5+2" />',
+      expected: "7",
+      inputs: { rootSelector: "r-" },
+      expectedQueryCallArgs: [["5+2"]],
+    },
+    {
+      name: "<set- foo=...>",
+      input: '<span><set- foo="5+2" /><r- content="foo" /></span>',
+      expected: "<span>7</span>",
+      inputs: { rootSelector: "span" },
+      expectedQueryCallArgs: [["5+2"], ["foo"]],
+    },
+  ];
+  for (const { name, input, expected, inputs, expectedQueryCallArgs } of a) {
+    await t.step(name, () => {
+      const context = new Inputs();
+      const querySpy = spy((_) => 7);
+      for (const [key, value] of Object.entries(inputs)) {
+        context.Set(key, value);
+      }
+      const result = template(input, context, querySpy);
+      for (let i = 0; i < expectedQueryCallArgs.length; i++) {
+        assertEquals(querySpy.calls[i].args, expectedQueryCallArgs[i]);
+      }
+      assertSpyCalls(querySpy, expectedQueryCallArgs.length);
+      assertEquals(result, expected);
+    });
+  }
 });
